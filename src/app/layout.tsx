429ade2815fa09da07967612e6cbd602198b1b99
im4nu <victorx811@gmail.com>
2024-02-27 16:44:57 -0300

    improving metadata using gif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://omanu.tech"),
+  alternates: {
+    canonical: "/",
+  },
   title: "O manu | Engenheiro Front-end e web designer",
   description: "Desenvolvimento de sites e aplicativos web e mobile.",
   openGraph: {
-    images: "/images/preview-mobile.jpeg",
+    images: "/mobilePreview.gif",
   },
 };
 
@@ -22,11 +26,21 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <link rel="canonical" href="https://omanu.tech" />
+        <meta
+          property="og:image"
+          content="https://omanu.tech/mobilePreview.gif"
+        />
+
         <meta property="og:image" content="<generated>" />
         <meta property="og:image:type" content="<generated>" />
         <meta property="og:image:width" content="<generated>" />
         <meta property="og:image:height" content="<generated>" />
-        <meta property="og:image:alt" content="About Acme" />
+
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
       </head>
       <body
         className={`bg-main w-screen min-h-screen h-full flex ${[
